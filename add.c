a085f8e572258df7d153e31c38ac169dc5935d8f
Jeevitha <Jeevitha1209>
2023-01-18 12:02:36 +0530

    first commit

---
+++ add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a,b;
+    int sum;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("Enter b:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("Sum: %d",sum);
+}
